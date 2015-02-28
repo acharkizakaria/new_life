644c2936ec628324997b8b9c05cbceda4b66d73b
acharki.zakaria <acharki.zakaria@gmail.com>
2015-02-28 11:17:49 +0000

    security badge

--- lib/new_life.rb
+++ lib/new_life.rb
@@ -7,6 +7,17 @@ module NewLife
     puts "Yes, Go to bed!!"
   end
 
+  class TimeKeeper
+    def verbose_time
+      time = Time.now
+      minute = time.min
+      hour = time.hour % 12
+      meridian_indicator = time.hour < 12 ? 'AM' : 'PM'
+
+      "#{minute} minutes past #{hour} o'clock, #{meridian_indicator}"
+    end
+  end
+
 =begin
   class Sleep
     def self.time_to_sleep?
